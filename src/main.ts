232822c59f04daad12a76313143b9efb37660a3b
MisaSM <misaelsarabiamolina@gmail.com>
2024-11-05 19:53:54 -0700

    Arreglados errores pequeños

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,12 @@ import { appRouterProviders } from './app/app.routes';
 import { importProvidersFrom } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 provideHttpClient
 bootstrapApplication(AppComponent, {
   providers: [
     appRouterProviders,
     provideHttpClient(),
-    importProvidersFrom(RouterModule),
-    BrowserAnimationsModule   // Esto asegurará que el routing esté disponible
+    importProvidersFrom(RouterModule, BrowserAnimationsModule) // Esto asegurará que el routing esté disponible
   ]
 }).catch(err => console.error(err));
